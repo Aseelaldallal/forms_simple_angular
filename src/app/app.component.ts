53203e820bbfcdcb223388f78b8d253f2225a6ff
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-09-20 22:44:42 -0400

    attempt 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  signupForm : FormGroup;
+  invalidName = 'test';
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'projectName': new FormControl(null, this.forbiddenNames),
+      'email': new FormControl(null, Validators.email, this.forbiddenEmails)
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if(control.value === 'test') {
+      return { 'nameIsForbidden' : true };
+    }
+    return null; 
+  }
+
+  forbiddenEmails(control: FormControl)  : Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(()=> {
+        if(control.value === 'test@gmail.com') {
+          resolve({
+            'emailIsForbidden': true
+          })
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    })
+  }
+
 }
